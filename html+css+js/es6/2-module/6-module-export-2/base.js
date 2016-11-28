b070b84ab6bb4dadee3232627b0423bda48f11a4
fangzi <szfangziwei@qq.com>
2016-11-28 22:56:31 +0800

    first commit

--- html+css+js/es6/2-module/6-module-export-2/base.js
+++ html+css+js/es6/2-module/6-module-export-2/base.js
@@ -1,4 +1,9 @@
 import {id} from 'module1.js';
+import {product} from 'module2.js';
 //报错，id是只读的
-id = 8000;
-console.log('我从module1.js里面加载到id：'+id);
+//id = 8000;
+//console.log('我从module1.js里面加载到id：'+id);
+
+//不报错，允许修改对象里面的属性
+product.id = 8000;
+console.log('我从module2.js里面加载到id：'+product.id);
