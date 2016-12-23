03f2744d82fba7ef4493bbcfd4f626dbe15e90f7
fang <test@qq.com>
2016-12-24 05:56:08 +0800

    s

--- nodejs/network/http.server.js
+++ nodejs/network/http.server.js
@@ -2,7 +2,19 @@ var http = require('http');
 
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type':'text/plain'});
-  res.end('Hi\n');
+  var buffers = [];
+  req.on('data', function (trunk) {
+    buffers.push(trunk);
+    console.log(trunk);
+  }).on('end', function () {
+    var buffer = Buffer.concat(buffers);
+    res.end(buffer);
+  })
+  if(req.url == '/'){
+    res.end('这是首页');
+  }else if(req.url == '/list'){
+    res.end('这是列表页');
+  }
 
 }).listen(1337);
 console.log('Server runing at http://localhost:1337/');
